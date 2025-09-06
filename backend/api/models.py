cc8428c12e97676904fe38f60905cb8ff403ef67
Steven-Situ <stevensitu7488@gmail.com>
2025-09-06 18:17:15 -0400

    commit

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth.models import User
+
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    Content = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
+
+
+def _str_(self):
+        return self.title
